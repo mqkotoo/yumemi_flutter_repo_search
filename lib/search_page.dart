cc5590d66e85c29bd7227343c78eb0f7bc15dd20
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2023-04-01 21:05:47 +0900

    Implement http communication

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -1,13 +1,23 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:yumemi_flutter_repo_search/repository/data_repository.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('search page'),
+        child: ElevatedButton(
+            onPressed: () async{
+              final result = await DataRepository().getData('flutter');
+              if (kDebugMode) {
+                print(result);
+              }
+            },
+            child: const Text('test'),
+        ),
       ),
     );
   }
