aba7a6268be9b52db336b795146f75ea25dc7ffa
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2023-04-01 16:09:52 +0900

    create data model

---
+++ lib/domain/repo_data_model.dart
@@ -0,0 +1,44 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'repo_data_model.freezed.dart';
+part 'repo_data_model.g.dart';
+
+@freezed
+class RepoDataModel with _$RepoDataModel {
+  const factory RepoDataModel({
+    required int totalCount,
+    required List<RepoDataItems> items,
+  }) = _RepoDataModel;
+
+  factory RepoDataModel.fromJson(Map<String, dynamic> json) =>
+      _$RepoDataModelFromJson(json);
+
+}
+
+@freezed
+class RepoDataItems with _$RepoDataItems {
+  const factory RepoDataItems({
+    required String fullName,
+    required String? description,
+    required String? language,
+    required int stargazersCount,
+    required int watchersCount,
+    required int forksCount,
+    required int openIssuesCount,
+    required RepoDataOwner owner,
+  }) = _RepoDataItems;
+
+  factory RepoDataItems.fromJson(Map<String, dynamic> json) =>
+      _$RepoDataItemsFromJson(json);
+}
+
+@freezed
+class RepoDataOwner with _$RepoDataOwner {
+  const factory RepoDataOwner({
+    required String avatarUrl,
+  }) = _RepoDataOwner;
+
+  factory RepoDataOwner.fromJson(Map<String, dynamic> json) =>
+      _$RepoDataOwnerFromJson(json);
+}
