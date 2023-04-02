272af78b9afde455e6ba4279f2b91e2619917925
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2023-04-02 15:18:00 +0900

    display info elements

--- lib/presentation/detail/detail_page.dart
+++ lib/presentation/detail/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../../domain/repo_data_model.dart';
 
@@ -9,14 +10,31 @@ class DetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // データの数をカンマ区切りで表示
+    final starsCount = NumberFormat('#,##0').format(repoData.stargazersCount);
+    final watchersCount = NumberFormat('#,##0').format(repoData.watchersCount);
+    final forksCount = NumberFormat('#,##0').format(repoData.forksCount);
+    final issuesCount = NumberFormat('#,##0').format(repoData.openIssuesCount);
     return Scaffold(
       appBar: AppBar(
-        title: Text('詳細ページ'),
+        title: const Text('詳細ページ'),
       ),
       body: Column(
         children: [
+          ClipOval(
+            child: Image.network(
+              repoData.owner.avatarUrl,
+              width: 120,
+              height: 120,
+            ),
+          ),
           Text(repoData.fullName),
-          Text(repoData.forksCount.toString()),
+          Text(repoData.description ?? 'No Description'),
+          Text(repoData.language ?? 'No Language'),
+          Text('star: $starsCount'),
+          Text('watch: $watchersCount'),
+          Text('fork: $forksCount'),
+          Text('issue: $issuesCount'),
         ],
       ),
     );
