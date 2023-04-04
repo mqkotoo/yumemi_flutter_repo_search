1b7ec485b2ef4eacfa5976a20b86d471779994d1
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2023-04-04 16:49:55 +0900

    add theme store test

---
+++ test/theme/theme_store_test.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:yumemi_flutter_repo_search/theme/shared_preferences_provider.dart';
+import 'package:yumemi_flutter_repo_search/theme/theme_mode_provider.dart';
+
+void main() {
+  group('themeに関するテスト', () {
+    late ProviderContainer container;
+    // SharedPreferences保存用キー
+    const isDarkThemeKey = 'selectedThemeKey';
+
+    setUp(() => () {
+          return Future(() async {
+            SharedPreferences.setMockInitialValues({});
+            container = ProviderContainer(overrides: [
+              sharedPreferencesProvider.overrideWithValue(
+                await SharedPreferences.getInstance(),
+              ),
+            ]);
+          });
+        });
+
+    tearDown(() => () {
+          container.dispose();
+        });
+
+    test('初期状態でテーマモードが端末のシステム依存であること', () async {
+      SharedPreferences.setMockInitialValues({});
+      container = ProviderContainer(overrides: [
+        sharedPreferencesProvider.overrideWithValue(
+          await SharedPreferences.getInstance(),
+        ),
+      ]);
+      final themeMode = container.read(themeModeProvider);
+      expect(themeMode, ThemeMode.system);
+    });
+
+    test('テーマが記憶されていたら、そのテーマモードになっていること', () async {
+      const isDark = false;
+      //テーマモードをライトモードに設定
+      SharedPreferences.setMockInitialValues({isDarkThemeKey: isDark});
+
+      container = ProviderContainer(overrides: [
+        sharedPreferencesProvider.overrideWithValue(
+          await SharedPreferences.getInstance(),
+        ),
+      ]);
+
+      final themeMode = container.read(themeModeProvider);
+      expect(themeMode, ThemeMode.light);
+    });
+
+    test('darkテーマに切り替え、テーマモードの保存ができていること', () async {
+      SharedPreferences.setMockInitialValues({});
+      container = ProviderContainer(overrides: [
+        sharedPreferencesProvider.overrideWithValue(
+          await SharedPreferences.getInstance(),
+        ),
+      ]);
+
+      final themeNotifier = container.read(themeModeProvider.notifier);
+      //toggle theme method →isDarkにtrueを入れてダークモードに変更
+      await themeNotifier.toggleThemeAndSave(true);
+      //themeNotifier.debugState　→　container.read(themeModeProvider); と同義
+      //テーマが切り替わっていることのテスト
+      expect(themeNotifier.debugState, ThemeMode.dark);
+
+      //テーマを管理しているBOOLが保存されているかのテスト
+      final prefs = container.read(sharedPreferencesProvider);
+      final theme = prefs.getBool(isDarkThemeKey);
+      expect(theme, true);
+    });
+
+    test('テーマが保存されていない状態で、テーマ切り替えスイッチが正常に表示されている', () async {
+      SharedPreferences.setMockInitialValues({});
+      container = ProviderContainer(overrides: [
+        sharedPreferencesProvider.overrideWithValue(
+          await SharedPreferences.getInstance(),
+        ),
+      ]);
+
+      final themeNotifier = container.read(themeModeProvider.notifier);
+      //テーマ切り替えはしない
+      // await themeNotifier.toggleThemeAndSave(true);
+      //themeNotifier.debugState　→　container.read(themeModeProvider); と同義
+
+      //テーマが初期状態であることのテスト
+      final themeModeState = themeNotifier.debugState;
+      expect(themeModeState, ThemeMode.system);
+
+      //システムのテーマと、スイッチの表示が一致しているか
+      //systemのテーマモードをlightだとする↓
+      final bool initialSwitchBool = themeModeState == ThemeMode.system
+          ? ThemeMode.light == ThemeMode.dark
+          : themeModeState == ThemeMode.dark;
+
+      //switchのフラグはisDarkなのでsystemがlightの場合switchはfalseが期待される
+      expect(initialSwitchBool, false);
+    });
+  });
+}
