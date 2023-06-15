f8fa08132bde63a7afb71bdb96a838a6fea0ed2c
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2023-06-15 17:15:11 +0900

    stateNotifier -> Notifier

--- lib/theme/theme_mode_provider.dart
+++ lib/theme/theme_mode_provider.dart
@@ -8,30 +8,30 @@ import 'package:yumemi_flutter_repo_search/theme/shared_preferences_provider.dar
 const _isDarkThemeKey = 'selectedThemeKey';
 
 class ThemeSelector extends Notifier<ThemeMode> {
-  ThemeSelector(this._ref);
-
   @override
   ThemeMode build() {
+    // prefインスタンス取得
+    final prefs = ref.read(sharedPreferencesProvider);
+
     // テーマが保存されていればテーマを反映、そうでなければ初期値のシステム依存
-    final isDarkTheme = _prefs.getBool(_isDarkThemeKey);
+    final isDarkTheme = prefs.getBool(_isDarkThemeKey);
+
     if (isDarkTheme == null) {
       return ThemeMode.system;
     }
     return isDarkTheme ? ThemeMode.dark : ThemeMode.light;
   }
 
-  final Ref _ref;
-
-  // 選択したテーマを保存するためのローカル保存領域
-  late final _prefs = _ref.read(sharedPreferencesProvider);
-
   // テーマの変更と保存
   Future<void> toggleThemeAndSave(bool isDarkTheme) async {
-    await _prefs.setBool(_isDarkThemeKey, isDarkTheme);
+    // prefインスタンス取得
+    final prefs = ref.read(sharedPreferencesProvider);
+
+    await prefs.setBool(_isDarkThemeKey, isDarkTheme);
     state = isDarkTheme ? ThemeMode.dark : ThemeMode.light;
   }
 }
 
-final themeModeProvider = NotifierProvider<ThemeSelector, ThemeMode>((ref) {
-  return ThemeSelector(ref);
-});
+final themeModeProvider = NotifierProvider<ThemeSelector, ThemeMode>(
+  ThemeSelector.new,
+);
