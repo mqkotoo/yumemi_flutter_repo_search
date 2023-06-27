6506b93f3ac67ac62cb9fda91388582e4dc5d71c
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2023-06-27 21:50:34 +0900

    add error class

---
+++ lib/domain/error.dart
@@ -0,0 +1,24 @@
+//以下はSAMPLEで入れているだけ
+class NoInternetException {
+  String message;
+
+  NoInternetException(this.message);
+}
+
+class NoServiceFoundException {
+  String message;
+
+  NoServiceFoundException(this.message);
+}
+
+class InvalidFormatException {
+  String message;
+
+  InvalidFormatException(this.message);
+}
+
+class UnknownException {
+  String message;
+
+  UnknownException(this.message);
+}
