e7f90d1f32c6d3f0160b779ac2641eb8dac326d2
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2023-07-01 23:56:21 +0900

    edit error class

--- lib/domain/error.dart
+++ lib/domain/error.dart
@@ -1,29 +1,19 @@
 //以下はSAMPLEで入れているだけ
 class NoInternetException {
-  String title;
   String message;
 
-  NoInternetException(this.title, this.message);
-}
-
-class NoResultException {
-  String title;
-  String message;
-
-  NoResultException(this.title, this.message);
+  NoInternetException(this.message);
 }
 
 class NoTextException {
-  String title;
   String message;
 
-  NoTextException(this.title, this.message);
+  NoTextException(this.message);
 }
 
 //その他のエラー（このアプリではリクエスト過多が基本）
 class UnknownException {
-  String title;
   String message;
 
-  UnknownException(this.title, this.message);
+  UnknownException(this.message);
 }
