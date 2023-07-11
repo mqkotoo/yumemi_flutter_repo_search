9d99961ae3cdf5a23c4d78011cb4ce9985ca48e0
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2023-07-11 12:41:36 +0900

    add search state

---
+++ lib/domain/search_state.dart
@@ -0,0 +1,28 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:yumemi_flutter_repo_search/domain/repo_data_model.dart';
+
+part 'search_state.freezed.dart';
+
+@freezed
+class SearchState with _$SearchState {
+  const factory SearchState.uninitialized() = SearchStateUninitialized;
+
+  const factory SearchState.searching() = SearchStateSearching;
+
+  const factory SearchState.success({
+    required List<RepoDataModel> repoData,
+    required String query,
+    required int page,
+    required bool hasNext,
+  }) = SearchStateSuccess;
+
+  const factory SearchState.failure({
+    required Exception exception,
+  }) = SearchStateFail;
+
+  const factory SearchState.fetchingNext({
+    required List<RepoDataModel> repoData,
+    required String query,
+    required int page,
+  }) = SearchStateFetchingNext;
+}
