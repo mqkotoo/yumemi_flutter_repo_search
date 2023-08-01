7f46d4c797eea8d159e83fe2d194934f37fe1a72
mqkotoo <87256037+mqkotoo@users.noreply.github.com>
2023-08-01 09:28:58 +0900

    update theme_mode_provider

--- lib/theme/theme_mode_provider.dart
+++ lib/theme/theme_mode_provider.dart
@@ -13,13 +13,14 @@ final themeModeProvider = NotifierProvider<ThemeSelector, ThemeMode>(
 );
 
 class ThemeSelector extends Notifier<ThemeMode> {
-  // prefインスタンス取得
-  late final prefs = ref.read(sharedPreferencesProvider);
+  late dynamic _prefs;
 
   @override
   ThemeMode build() {
+    //現在のテーマを取得
+    _prefs = ref.read(sharedPreferencesProvider);
     // テーマが保存されていればテーマを反映、そうでなければ初期値のシステム依存
-    final isDarkTheme = prefs.getBool(_isDarkThemeKey);
+    final isDarkTheme = _prefs.getBool(_isDarkThemeKey);
     if (isDarkTheme == null) {
       return ThemeMode.system;
     }
@@ -28,7 +29,7 @@ class ThemeSelector extends Notifier<ThemeMode> {
 
   // テーマの変更と保存
   Future<void> toggleThemeAndSave(bool isDarkTheme) async {
-    await prefs.setBool(_isDarkThemeKey, isDarkTheme);
+    await _prefs.setBool(_isDarkThemeKey, isDarkTheme);
     state = isDarkTheme ? ThemeMode.dark : ThemeMode.light;
   }
 }
